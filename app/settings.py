1771b4fb5e44b71909eb936fae7b366f24731efe
Christian Hess <christianhess.rlz@gmail.com>
2020-01-15 22:55:30 -0300

    Fix wizard settings by removing deprecated panel

--- app/settings.py
+++ app/settings.py
@@ -191,8 +191,7 @@ SHUUP_SETUP_WIZARD_PANE_SPEC = [
     "shuup.admin.modules.service_providers.views.PaymentWizardPane",
     "shuup.admin.modules.service_providers.views.CarrierWizardPane",
     "shuup.xtheme.admin_module.views.ThemeWizardPane",
-    "shuup.admin.modules.content.views.ContentWizardPane",
-    "shuup.admin.modules.sample_data.views.SampleObjectsWizardPane",
+    "shuup.testing.modules.sample_data.views.SampleObjectsWizardPane" if DEBUG else "",
     "shuup.admin.modules.system.views.TelemetryWizardPane"
 ]
 
